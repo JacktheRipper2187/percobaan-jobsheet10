adc9f4d994f3a85a1ef709fa1b2e9936c6279ac6
JackTheRipper2187 <ulilf442@gmail.com>
2023-11-21 22:39:41 +0700

    modifikasi agar setiap baris array sama

--- Numbers22.java
+++ Numbers22.java
@@ -1,12 +1,10 @@
 public class Numbers22 {
     public static void main(String[] args) {
-        int[][] myNumbers = new int[3][];
-        myNumbers[0] = new int[5];
-        myNumbers[1] = new int[3];
-        myNumbers[2] = new int[1];
+        int maxLength = 5; // Menentukan panjang maksimal untuk setiap baris
+        int[][] myNumbers = new int[3][maxLength];
 
         for (int i = 0; i < myNumbers.length; i++) {
-            System.out.println ("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);   
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + myNumbers[i].length);
         }
     }
 }
